6d3d64aa3ebf74a5a82cdcc64368aa8191b57602
Vinicius Teixeira Saraiva <vinicius.saraiva@sgs.com>
2023-03-21 23:00:49 -0300

    working with leaflet maps lib

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={ <Home/> }/>
 				<Route path="/login" element={ <Login/> }/>
-				<Route path="/listenings" element={ <Listings/> }/>
+				<Route path="/listings" element={ <Listings/> }/>
 			</Routes>
 		</div>
 	);
--- frontend/src/components/listings/Listings.jsx
+++ frontend/src/components/listings/Listings.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
 
-function Listings(props) {
+import {
+	MapContainer,
+	Marker,
+	Popup,
+	TileLayer
+} from 'react-leaflet'
+
+import { Grid, AppBar, Typography } from '@mui/material'
+import {Icon} from "leaflet"
+
+import houseIconPng from "../../assets/Mapicons/house.png"
+import apartmentIconPng from "../../assets/Mapicons/apartment.png"
+import officeIconPng from "../../assets/Mapicons/office.png"
+
+function Listings() {
+	const houseicon = new Icon({
+		iconUrl: houseIconPng,
+		iconSize: [40, 40],
+	                           })
+	
+	const apartmentIcon = new Icon({
+		                           iconUrl: apartmentIconPng,
+		                           iconSize: [40, 40],
+	                           })
+	
+	const officeIcon = new Icon({
+		                               iconUrl: officeIconPng,
+		                               iconSize: [40, 40],
+	                               })
+	
 	return (
-		<div></div>
+		<Grid container>
+			<Grid item xs={4}>
+				<Typography>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
+			</Grid>
+			<Grid item xs={8}>
+				<AppBar position={'sticky'}>
+					<div style={{height: '100vh'}}>
+						<MapContainer center={[-22.951955502815867, -43.210326270667416]} zoom={44} scrollWheelZoom={true}>
+							<TileLayer
+								attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+								// url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+								url={"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"}
+							/>
+							<Marker icon={houseicon} position={[-22.951955502815867, -43.210326270667416]}>
+								{/*<Popup>*/}
+								{/*	A pretty CSS3 popup. <br /> Easily customizable.*/}
+								{/*</Popup>*/}
+							</Marker>
+						</MapContainer>
+					</div>
+				</AppBar>
+			</Grid>
+		</Grid>
 	);
 }
 
