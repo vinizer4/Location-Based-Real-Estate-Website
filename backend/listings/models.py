61401040dfec683c2b80c13dd3ce9f784b496602
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-03-23 12:10:07 -0300

    feat: creating property listings in the admin panel

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -66,3 +66,6 @@ class Listing(models.Model):
 	parking = models.BooleanField(default=False)
 	date_posted = models.DateTimeField(default=timezone.now)
 	location = models.PointField(blank=True, null=True, srid=4326)
+	
+	def __str__(self):
+		return self.title
